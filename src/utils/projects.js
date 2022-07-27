bc556971f219b644362148c9dd8c48018d78a1da
leonpibe <steven.deleonfeliz@gmail.com>
2022-07-27 12:22:59 +0800

    agregando proyecto piedra papel o tijeras

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,5 +1,6 @@
 import jonmirchaChalleng from "../images/proyectos/jonchalleng.png"
 import cssdegradado from "../images/proyectos/degradadocss.png"
+import piedrapapel from "../images/proyectos/gamephoto.png"
 
 export default [
   {
@@ -15,6 +16,13 @@ export default [
       "Aca les comparto un simple codigo de css para lo que quieran hacer un bello background solo usando propiedades de css, aparte les dejo el link de una pagina donde podran ir configurando el background que mas les guste",
     url: "https://github.com/stevenfeliz/css_background_degradado",
     image: cssdegradado,
+  },
+  {
+    title: "Piedra papel o tijeras - javascript",
+    description:
+      "Les comparto una simple práctica con javascript vanilla el juego piedra papel o tijeras!",
+    url: "https://github.com/stevenfeliz/piedra-papel-o-tijeras---javascript",
+    image: piedrapapel,
   },
   
   
