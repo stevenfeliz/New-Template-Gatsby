876963948d49d64b54c0aa97580e4f4f5409f259
sdeleon <steven.deleonfeliz@gmail.com>
2023-05-03 23:22:28 -0400

    se edito el about me

--- src/components/Aboutme/Aboutme.js
+++ src/components/Aboutme/Aboutme.js
@@ -12,7 +12,7 @@ export default function Aboutme() {
         
         <h1 className='text-center'>About me</h1>
 <div className='box text-center'>
-    <p>Solid front-end knowledge of HTML, CSS, Javascript, React, Gatsby. Experience in Python with Selenium and library Requests, Regex. Lolixscript with Openbullet opensource webscrapy tools Strong understanding of design principles, UI/UX, Modular CSS, Bootstrap, Grid layout and responsive design. Positive attitude, strong work ethic, out-of-the-box thinking and high level of professional integrity. Salmos 128:2 ✞</p>
+    <p>Solid front-end knowledge of HTML, CSS, Javascript, React, Gatsby. Experience in Python with Selenium and library Requests, Regex. webscrapy tools Strong understanding of design principles, UI/UX, Modular CSS, Bootstrap, Grid layout and responsive design. Positive attitude, strong work ethic, out-of-the-box thinking and high level of professional integrity. Salmos 128:2 ✞</p>
 
     <hr />
     <a href={CV} target="_blank">
